8c4b66ab06b035308b82d7b58902d5862cd17887
SujithaKC <sujithakanagarathinam@gmail.com>
2024-08-28 21:37:54 +0530

    Added functionality for updating data

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,49 +1,54 @@
-import React from 'react'
+import React from "react";
 import { Container, Form, Button } from "react-bootstrap";
-import "../styles/Login.css" ;
+import "../styles/Login.css";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-    const [formData, setFormData] = useState({
-    
-        email: "",
-        password: ""
-
-    })
-    const handleChange = () => {
-
-    }
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
     <Container>
-        <h1>Login Form</h1>
-        <Form>
-            
-            <Form.Group>
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                    type="text"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange} required
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    type="password"
-                    name="password"
-                    value={formData.password}
-                    onChange={handleChange} required
-                />
-            </Form.Group>
-            <Button variant='success' type='submit'>Register</Button>
-            {/* <p>Already have an account?</p> <Link to ="/login">Login</Link> */}
-        </Form>
+      <h1>Login Form</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="text"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+        <Button variant="success" type="submit">
+          Register
+        </Button>
+        {/* <p>Already have an account?</p> <Link to ="/login">Login</Link> */}
+      </Form>
     </Container>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
